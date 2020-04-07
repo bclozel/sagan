abbd048e220aabdca1eae0be9c5b7d68537b48f9
Dave Syer <dsyer@pivotal.io>
2020-04-07 15:30:07 +0200

    Refactor SaganApplication after Spring Boot upgrade

--- sagan-site/src/main/java/sagan/SaganApplication.java
+++ sagan-site/src/main/java/sagan/SaganApplication.java
@@ -7,9 +7,11 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.Profiles;
 
 import static java.lang.String.format;
 import static org.springframework.util.StringUtils.arrayToCommaDelimitedString;
@@ -21,40 +23,53 @@ import static sagan.SaganProfiles.*;
  */
 public class SaganApplication extends SpringApplication {
 
-    private static final Log logger = LogFactory.getLog(SaganApplication.class);
+	private static final Log logger = LogFactory.getLog(SaganApplication.class);
 
-    public SaganApplication(Class<?> configClass) {
-        super(configClass);
-    }
+	public SaganApplication(Class<?> configClass) {
+		super(configClass);
+	}
 
-    /**
-     * Enforce mutual exclusivity and implicit activation of profiles as described in
-     * {@link SaganProfiles}.
-     */
-    @Override
-    protected void configureProfiles(ConfigurableEnvironment environment, String[] args) {
-        super.configureProfiles(environment, args);
+	public static SpringApplication create(Class<?> configClass) {
+		try {
+			return new SaganApplication(configClass);
+		} catch (Throwable e) {
+			try {
+				return BeanUtils.instantiateClass(SpringApplication.class.getConstructor(Object[].class), new Object[] {
+						new Object[] {
+								configClass.getName() } });
+			} catch (Exception ex) {
+				throw new IllegalStateException(ex);
+			}
+		}
+	}
 
-        boolean stagingActive = environment.acceptsProfiles(STAGING);
-        boolean productionActive = environment.acceptsProfiles(PRODUCTION);
+	/**
+	 * Enforce mutual exclusivity and implicit activation of profiles as described in
+	 * {@link SaganProfiles}.
+	 */
+	@Override
+	protected void configureProfiles(ConfigurableEnvironment environment, String[] args) {
+		super.configureProfiles(environment, args);
 
-        if (stagingActive && productionActive) {
-            throw new IllegalStateException(format("Only one of the following profiles may be specified: [%s]",
-                    arrayToCommaDelimitedString(new String[] { STAGING, PRODUCTION })));
-        }
+		boolean stagingActive = environment.acceptsProfiles(Profiles.of(STAGING));
+		boolean productionActive = environment.acceptsProfiles(Profiles.of(PRODUCTION));
 
-        if (stagingActive || productionActive) {
-            logger.info(format("Activating '%s' profile because one of '%s' or '%s' profiles have been specified.",
-                    CLOUDFOUNDRY, STAGING, PRODUCTION));
-            environment.addActiveProfile(CLOUDFOUNDRY);
-        }
-        else {
-            logger.info("The default 'standalone' profile is active because no other profiles have been specified.");
-            environment.addActiveProfile(STANDALONE);
-            Map<String, Object> map = new HashMap<>();
-            map.put("client.dir", new File("../sagan-client").getAbsolutePath());
-            environment.getPropertySources().addLast(new MapPropertySource("clientDir", map));
-        }
+		if (stagingActive && productionActive) {
+			throw new IllegalStateException(format("Only one of the following profiles may be specified: [%s]",
+					arrayToCommaDelimitedString(new String[] { STAGING, PRODUCTION })));
+		}
 
-    }
+		if (stagingActive || productionActive) {
+			logger.info(format("Activating '%s' profile because one of '%s' or '%s' profiles have been specified.",
+					CLOUDFOUNDRY, STAGING, PRODUCTION));
+			environment.addActiveProfile(CLOUDFOUNDRY);
+		} else {
+			logger.info("The default 'standalone' profile is active because no other profiles have been specified.");
+			environment.addActiveProfile(STANDALONE);
+			Map<String, Object> map = new HashMap<>();
+			map.put("client.dir", new File("../sagan-client").getAbsolutePath());
+			environment.getPropertySources().addLast(new MapPropertySource("clientDir", map));
+		}
+
+	}
 }
